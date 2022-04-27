3e39003693d32414edede1a1375568ae2201d4fd
cancer <zodiac.cancer.j6@gmail.com>
2022-04-27 18:08:44 +0900

    add 01-5

---
+++ 01-5/main.js
@@ -0,0 +1,9 @@
+const promiseA = Promise.resolve("a");
+const promiseB = new Promise((_, reject) =>
+  setTimeout(() => reject(new Error("b rejected")), 1000)
+);
+const promiseC = new Promise((resolve) => setTimeout(() => resolve("c"), 2000));
+
+Promise.allSettled([promiseA, promiseB, promiseC])
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
