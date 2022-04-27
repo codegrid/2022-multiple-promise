6c19614058e2f4fdeb11ae3ab52db59aa54ba974
cancer <zodiac.cancer.j6@gmail.com>
2022-04-27 18:23:50 +0900

    add 01-11

---
+++ 01-11/main.js
@@ -0,0 +1,11 @@
+const promiseA = Promise.reject(new Error("a failed"));
+const promiseB = new Promise((resolve) =>
+  setTimeout(() => resolve("b"), 1000)
+);
+const promiseC = new Promise((resolve) =>
+  setTimeout(() => resolve("c"), 2000)
+);
+
+Promise.race([promiseA, promiseB, promiseC])
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
