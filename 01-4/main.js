2c98117665aa56761bc1f1a8b706c4b393b8140a
cancer <zodiac.cancer.j6@gmail.com>
2022-04-27 18:04:28 +0900

    add 01-4

---
+++ 01-4/main.js
@@ -0,0 +1,7 @@
+const promiseA = Promise.resolve("a");
+const promiseB = new Promise((resolve) => setTimeout(() => resolve("b"), 1000));
+const promiseC = new Promise((resolve) => setTimeout(() => resolve("c"), 2000));
+
+Promise.allSettled([promiseA, promiseB, promiseC]).then((result) =>
+  console.log(result)
+);
