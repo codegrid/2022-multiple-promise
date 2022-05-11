0c655bcf1d4e3b9b83780f5daef70f8e94a984f2
cancer <zodiac.cancer.j6@gmail.com>
2022-05-11 12:38:13 +0900

    fix error message

--- 01-2/main.js
+++ 01-2/main.js
@@ -1,6 +1,6 @@
 const promiseA = Promise.resolve("a");
 const promiseB = new Promise((_, reject) =>
-  setTimeout(() => reject(new Error("b rejected")), 1000)
+  setTimeout(() => reject(new Error("b failed")), 1000)
 );
 const promiseC = new Promise((resolve) =>
   setTimeout(() => resolve("c"), 2000)
